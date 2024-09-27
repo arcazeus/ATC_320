26b4c915170ea15cec52dccf0200d048b04966a5
peaches <peizhe2001@hotmail.com>
2024-09-27 13:06:22 -0400

    Add various operator command functions & created Command class to represent aircraft command

--- src/Operator.cpp
+++ src/Operator.cpp
@@ -1,1 +1,43 @@
+#include <Operator.h>
+#include <iostream>
+
+/////Constructors/////
+// Constructor for Command
+Command::Command(const std::string& id, double speedX, double speedY, double speedZ, double altitude)
+    : aircraftID(id), newSpeedX(speedX), newSpeedY(speedY), newSpeedZ(speedZ), newAltitude(altitude) {}
+
+// OperatorConsole constructor
+OperatorConsole::OperatorConsole() {
+    // Initialize the console
+}
+
+/////Aircraft related commands/////
+//send command to aircraft
+void OperatorConsole::sendCommand(const Command& cmd) {
+    std::cout << "Sending command to aircraft " << cmd.aircraftID << ":\n";
+    std::cout << "New Speed (X, Y, Z): " << cmd.newSpeedX << ", " << cmd.newSpeedY << ", " << cmd.newSpeedZ << "\n";
+    std::cout << "New Altitude: " << cmd.newAltitude << "\n";
+    communicateWithSystem(cmd);
+}
+
+//request aircraft info
+std::string OperatorConsole::requestAircraftInfo(const std::string& aircraftID) {
+    std::cout << "Requesting info for aircraft " << aircraftID << "\n";
+    // Should involve a request to the computer system
+    // For now, return something temporary
+    return "Aircraft ID: " + aircraftID + ", Speed: (500, 200, 0), Altitude: 35000";
+}
+
+//display operator console logs
+void OperatorConsole::displayConsole() {
+    std::cout << "Operator Console Display\n";
+    std::cout << "Commands and requests can be sent from here.\n";
+}
+
+//function to communicate with system
+void OperatorConsole::communicateWithSystem(const Command& cmd) {
+    // Simulate communication with the system for now
+    std::cout << "Command transmitted to system for processing.\n";
+}
+
 
--- src/Operator.h
+++ src/Operator.h
@@ -1,1 +1,34 @@
+#ifndef OPERATOR_CONSOLE_H
+#define OPERATOR_CONSOLE_H
 
+#include <string>
+#include <vector>
+
+
+// Class representing an aircraft command
+class Command {
+public:
+    std::string aircraftID;
+    double newSpeedX;
+    double newSpeedY;
+    double newSpeedZ;
+    double newAltitude;
+
+    Command(const std::string& id, double speedX, double speedY, double speedZ, double altitude);
+};
+
+class Operator{
+private:
+	void communicateWithSystem(const Command& cmd);
+
+public:
+	Operator();
+
+	void sendCommand(const Command& cmd);
+
+	std::string requestAircraftInfo(const std::string& aircraftID);
+
+	void displayConsole();
+};
+
+#endif
